5ebe259ea1f91375df7c7ac64b089fe9b77ece26
relente-patriciajoy <relente.patriciajoy@gmail.com>
2024-10-11 10:14:09 +0800

    Upload exe3.py to playground

---
+++ exe3.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+highest_number = max(num1,num2,num3)
+
+print("The highest number is: ", highest_number)
