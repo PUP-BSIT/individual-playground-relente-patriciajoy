346bf6ec77b17cdb16ca7537d0f5dacff98adcb6
relente-patriciajoy <relente.patriciajoy@gmail.com>
2024-10-12 11:36:29 +0800

    Update file

--- exe2.py
+++ exe2.py
@@ -1,9 +1,9 @@
 num = int(input("Enter a number: "))
 
-if num % 2 != 0:
+if num == 0:
+    print("Zero")
+elif num % 2 != 0:
     print("Odd number.")
-elif num % 2:
-    print("Even number.")
-elif num == 0:
-    print("Zero") 
+elif num % 2 == 0:
+    print("Even number.") 
     
