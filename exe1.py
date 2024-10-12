04c155b864c50cf3b2a3b85e7a18de8c1e5f6e86
relente-patriciajoy <relente.patriciajoy@gmail.com>
2024-10-12 11:01:38 +0800

    Update exe1.py file

--- exe1.py
+++ exe1.py
@@ -1,7 +1,7 @@
-a = input("Enter the first number: ")
-b = input("Enter the second number: ")
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
 
-if a == b:
+if num1 == num2:
   print ("Both inputs are equal.")
 else:
   print ("Your input is not equal.")
